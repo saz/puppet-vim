a30c3ac0170e98bbb12cb5a84f297baa508720e8
Saz <me@saz.sh>
2011-11-24 15:38:08 +0100

    only set vim as default editor when package is installed

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,7 +2,8 @@ class vim::config {
     case $operatingsystem {
         /(Ubuntu|Debian)/: {
             exec { "update-alternatives --set editor /usr/bin/${vim::params::editor_name}":
-                unless => "test /etc/alternatives/editor -ef /usr/bin/${vim::params::editor_name}",
+                unless  => "test /etc/alternatives/editor -ef /usr/bin/${vim::params::editor_name}",
+                require => Class['vim::install'],
             }
         }
     }
