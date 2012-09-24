622240edc75b3fae11156cef0fe11a5359795322
Steffen Zieger <me@saz.sh>
2012-09-24 11:46:43 +0200

    add freebsd support

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,14 +1,22 @@
 class vim::params {
-  case $::operatingsystem {
-    ubuntu, debian: {
+  case $::osfamily {
+    debian: {
       $package = 'vim-nox'
       $editor_name = 'vim.nox'
       $set_as_default = true
       $set_editor_cmd = "update-alternatives --set editor /usr/bin/${editor_name}"
       $test_editor_set = "test /etc/alternatives/editor -ef /usr/bin/${editor_name}"
     }
+    freebsd: {
+      $package = 'vim-lite'
+      $set_as_default = false
+    }
     default: {
-      fail("Unsupported platform: ${::operatingsystem}")
+      case $::operatingsystem {
+        default: {
+          fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
+        }
+      }
     }
   }
 }
