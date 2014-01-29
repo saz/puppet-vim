57ffbb7deec58ac1519c4c570678dc55efa79f58
Florent Poinsaut <florent.poinsaut@echoes-tech.com>
2014-01-29 15:29:04 +0100

    BugFix: 'ensure' attribute is not valid for file conf, happens when autoupgrade is enabled

--- manifests/init.pp
+++ manifests/init.pp
@@ -82,9 +82,11 @@ class vim(
       } else {
         $package_ensure = 'present'
       }
+      $file_ensure = 'file'
     }
     /(absent)/: {
       $package_ensure = 'absent'
+      $file_ensure = 'absent'
     }
     default: {
       fail('ensure parameter must be present or absent')
@@ -96,7 +98,7 @@ class vim(
   }
 
   file { $conf_file:
-    ensure  => $package_ensure,
+    ensure  => $file_ensure,
     content => template('vim/vimrc.erb'),
   }
 
