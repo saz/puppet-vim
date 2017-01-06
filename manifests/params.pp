5d993265774143137458981f26f6806393472cd5
Steffen Zieger <me@saz.sh>
2017-01-06 19:42:35 +0100

    improve testing, fix some lint errors

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class vim::params {
   $nocompatible = true
   $background   = 'dark'
-  $backspace    = "2"
+  $backspace    = '2'
   $lastposition = true
   $indent       = true
   $matchparen   = true
@@ -12,7 +12,7 @@ class vim::params {
   $maps         = {}
   $code         = []
   case $::osfamily {
-    debian: {
+    'Debian': {
       $package         = 'vim-nox'
       $editor_name     = 'vim.nox'
       $set_as_default  = true
@@ -20,28 +20,28 @@ class vim::params {
       $test_editor_set = "test /etc/alternatives/editor -ef /usr/bin/${editor_name}"
       $conf            = '/etc/vim/vimrc'
     }
-    redhat: {
+    'RedHat': {
       $package        = 'vim-enhanced'
       $set_as_default = false
       $conf           = '/etc/vimrc'
     }
-    freebsd: {
+    'FreeBSD': {
       $package        = 'vim-lite'
       $set_as_default = false
     }
-    suse: {
+    'Suse': {
       $package        = 'vim'
       $set_as_default = false
       $conf           = '/etc/vimrc'
     }
-    gentoo: {
+    'Gentoo': {
       $package         = 'app-editors/vim'
       $set_as_default  = true
       $set_editor_cmd  = 'eselect editor set /usr/bin/vim'
       $test_editor_set = 'eselect editor show|grep /usr/bin/vim'
       $conf            = '/etc/vimrc'
     }
-    solaris: {
+    'Solaris': {
       if($::operatingsystemrelease =~ /^(5\.11|11|11\.\d+)$/){
         $package        = '/editor/vim'
         $conf           = '/usr/share/vim/vimrc'
@@ -50,7 +50,6 @@ class vim::params {
         fail("vim::params: Unsupported platform: ${::osfamily}/${::operatingsystemrelease}")
       }
     }
-    
     default: {
       case $::operatingsystem {
         default: {
